c9ab360da6565b299f5a82bf0074b36a512fc1bd
Jake Vossen <jake@vossen.dev>
2019-07-06 09:21:53 -0600

    added name, started doing datetime math

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
+import datetime
+from datetime import timezone
 import csv
 
 def main() -> None:
-    getDataFromCsv()
+    tasks = getDataFromCsv()
+    x = 0
+    for t in tasks:
+        if (t.completionDate != "" and ((datetime.datetime.now(timezone.utc) - t.completionDate) < datetime.timedelta(days=7))):
+            print(t)
+            x = x + 1
+    print("Total: " + str(x))
 
 def getDataFromCsv() -> list:
     tasks = []
     with open('omnifocus-data/OmniFocus.csv') as csvDataFile:
         csvReader = csv.reader(csvDataFile)
         for row in csvReader:
-            tasks.append(Task(row[0], row[1], row[3], row[4], row[6], row[8]))
+            if (row[0] != "Task ID" and row[1] != "Inbox"): # This is to prevent getting the header row into the data
+                tasks.append(Task(row[0], row[1], row[2], row[3], row[4], row[6], row[8]))
     return tasks
 
     
     
 class Task:
-    def __init__(self, taskID, taskType, status, project, startDate, completionDate) -> None:
+    def __init__(self, taskID, taskType, name, status, project, startDate, completionDate) -> None:
         self.taskID = taskID
         self.taskType = taskType
+        self.name = name
         self.project = project
         self.status = status
         self.startDate = startDate # TODO convert to datetime
-        self.completionDate = completionDate
+        self.completionDate = datetime.datetime.strptime(completionDate, '%Y-%m-%d %H:%M:%S %z') if completionDate != "" else ""
     def __str__(self) -> str:
-        return "ID: " + self.taskID + "\n taskType: " + self.taskType + "\n status: " + self.status + "\n project: " + self.project + "\n status: " + self.status + "\n completionDate: " + self.completionDate
+        return " ID: " + self.taskID + "\n taskType: " + self.taskType + "\n name: " + self.name + "\n status: " + self.status + "\n project: " + self.project + "\n status: " + self.status + "\n completionDate: " + str(self.completionDate)
     
     
 main()
