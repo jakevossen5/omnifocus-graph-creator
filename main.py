6ea08ee11aa39ef61a60c5e8e49f2ddb60768df3
Jake Vossen <jake@vossen.dev>
2019-07-06 08:39:54 -0600

    added types, making list of tasks

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import csv
 
-def main():
-    print("Starting main")
+def main() -> None:
     getDataFromCsv()
 
-def getDataFromCsv():
+def getDataFromCsv() -> list:
+    tasks = []
     with open('omnifocus-data/OmniFocus.csv') as csvDataFile:
         csvReader = csv.reader(csvDataFile)
         for row in csvReader:
-            print(row)
+            tasks.append(Task(row[0], row[1], row[3], row[4], row[6], row[8]))
+    return tasks
+
     
     
 class Task:
-    def __init__(self, taskID, taskType, project, status, startDate, completionDate):
+    def __init__(self, taskID, taskType, status, project, startDate, completionDate) -> None:
         self.taskID = taskID
         self.taskType = taskType
         self.project = project
         self.status = status
         self.startDate = startDate # TODO convert to datetime
         self.completionDate = completionDate
-        
+    def __str__(self) -> str:
+        return "ID: " + self.taskID + "\n taskType: " + self.taskType + "\n status: " + self.status + "\n project: " + self.project + "\n status: " + self.status + "\n completionDate: " + self.completionDate
     
     
 main()
