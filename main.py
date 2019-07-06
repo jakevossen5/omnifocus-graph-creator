c23148c1e6636381b183f17484f9403d430dc87f
Jake Vossen <jake@vossen.dev>
2019-07-06 16:38:17 -0600

    added args + generalized for any number of days

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ import datetime
 from datetime import timezone
 import csv
 import matplotlib.pyplot as plt
+from sys import argv
+import argparse
+parser = argparse.ArgumentParser()
 
 def main() -> None:
-    tasks = get_data_from_csv()
-    x = 0
-    tasks_per_days_ago = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0}
+    parser.add_argument("-p", "--path-to-csv", dest="path_to_csv", help="Path to your OmniFocus CSV export")
+    parser.add_argument("-d", "--days", dest="days", help="The number of days you want to view", type=int)
+    args = parser.parse_args()
+    path_to_csv = args.path_to_csv
+    days = args.days + 1 # add 1 to be inclusive
+    tasks = get_data_from_csv(path_to_csv)
+    tasks_per_days_ago = { }
+    for i in range(days): # Do this as a lambda somehow?
+        tasks_per_days_ago[i] = 0
+
     now_no_tz = datetime.datetime.now()
     delta_start_of_day = now_no_tz - datetime.datetime(now_no_tz.year, now_no_tz.month, now_no_tz.day, 0, 0, 0, 0)
     # print("Delta start of day:", delta_start_of_day)
     # print("Adding to delta:", delta_start_of_day + datetime.timedelta(days = 1))
     for t in tasks:
-        for i in range(8):
+        for i in range(days):
             if (type(t.completionDate) == type(datetime.datetime.now(timezone.utc))):
                 time_difference = datetime.datetime.now(timezone.utc) - t.completionDate
                 # print("time_difference: ", time_difference)
@@ -20,23 +30,23 @@ def main() -> None:
                 # print("Second time difference:", (delta_start_of_day + datetime.timedelta(days=i + 1)))
                 if ((time_difference < delta_start_of_day + datetime.timedelta(days=i)) and (time_difference > delta_start_of_day + datetime.timedelta(days=i - 1))):
                     tasks_per_days_ago[i] += 1
-                    print(t)
+                    # print(t)
     print("Totals: " + str(tasks_per_days_ago))
 
-    create_plots(tasks_per_days_ago)
+    create_plots(tasks_per_days_ago, days)
 
-def create_plots(tasks_per_days_ago) -> None:
-    x = list(range(8))
+def create_plots(tasks_per_days_ago, days) -> None:
+    x = list(range(days))
     y = []
     for i in x:
-        y.append(tasks_per_days_ago[i])
+        y.append(tasks_per_days_ago[i]) 
     plt.scatter(x, y)
     plt.show()
     
     
-def get_data_from_csv() -> list:
+def get_data_from_csv(path) -> list:
     tasks = []
-    with open('omnifocus-data/OmniFocus.csv') as csvDataFile:
+    with open(path) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
         for row in csvReader:
             if (row[0] != "Task ID" and row[1] != "Inbox"): # This is to prevent getting the header row into the data
