b7f504806f545aab1455071e18a019a9cdcf680d
= <jake@vossen.dev>
2019-10-02 12:25:46 -0600

    starting reading csv, before serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::error::Error;
+use std::io;
+use std::process;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Task {
+    TaskID: String,
+    Type: Option<String>,
+    Name: Option<String>,
+    Status: Option<String>,
+}
+
+fn example() -> Result<(), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::ReaderBuilder::new()
+        .flexible(true)
+        .from_reader(io::stdin());
+    for result in rdr.records() {
+        // The iterator yields Result<StringRecord, Error>, so we check the
+        // error here.
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
