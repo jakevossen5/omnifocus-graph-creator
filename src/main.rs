faa1ed2bd77d441c9793370ab38c7290c4324bb7
= <jake@vossen.dev>
2019-10-11 14:36:39 -0600

    histogram working

--- src/main.rs
+++ src/main.rs
@@ -92,68 +92,62 @@ fn main() {
     println!("tasks size is {}", tasks.len());
 
     let days_requested: i32 = 7; // TODO: get this value from the user input / command line
-    let mut days_ago_to_task_count: HashMap<i32, i32> = HashMap::new();
+    let mut days_ago_to_task_count: HashMap<u32, u32> = HashMap::new();
     days_ago_to_task_count = map_tasks_to_days_ago(tasks);
 
-    let points: Vec<(i32, i32)> = get_points(days_ago_to_task_count, days_requested);
+    let points: Vec<u32> = get_points(days_ago_to_task_count, days_requested);
 
-    // draw_data();
+    draw_data(points, 10);
 }
 
-fn get_points(map: HashMap<i32, i32>, days_requested: i32) -> Vec<(i32, i32)> {
-    let mut points: Vec<(i32, i32)> = Vec::new();
+fn get_points(map: HashMap<u32, u32>, days_requested: i32) -> Vec<u32> {
+    let mut points: Vec<u32> = Vec::new();
     for x in 0..days_requested {
-        points.push((x, *map.get(&x).unwrap_or(&0)));
+        for j in 0..(*map.get(&(x as u32)).unwrap_or(&0)) {
+            points.push(x as u32);
+        }
     }
     return points;
 }
 
-fn draw_data(points: Vec<(i32, i32)>, max_point: i32) -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("stats.png", (640, 480)).into_drawing_area();
-    root.fill(&WHITE);
-    let root = root.margin(10, 10, 10, 10);
-    // After this point, we should be able to draw construct a chart context
-    let mut chart = ChartBuilder::on(&root)
-        // Set the caption of the chart
-        // .caption("This is our first plot", ("Arial", 40).into_font())
-        // Set the size of the label region
-        .x_label_area_size(20)
-        .y_label_area_size(40)
-        // Finally attach a coordinate on the drawing area and make a chart context
-        .build_ranged(0f32..10f32, 0f32..10f32)?;
+fn draw_data(points: Vec<u32>, max_point: i32) -> Result<(), Box<dyn std::error::Error>> {
+        let root =
+        BitMapBackend::new("stats.png", (640, 480)).into_drawing_area();
 
-    // Then we can draw a mesh
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .x_label_area_size(35)
+        .y_label_area_size(40)
+        .margin(5)
+        .caption("Histogram Test", ("Arial", 50.0).into_font())
+        .build_ranged(0u32..50u32, 0u32..50u32)?;
+
     chart
         .configure_mesh()
-        // We can customize the maximum number of labels allowed for each axis
-        .x_labels(5)
-        .y_labels(5)
-        // We can also change the format of the label text
-        .y_label_formatter(&|x| format!("{:.3}", x))
+        .disable_x_mesh()
+        .line_style_1(&WHITE.mix(0.3))
+        .x_label_offset(30)
+        .y_desc("Count")
+        .x_desc("Bucket")
+        .axis_desc_style(("Arial", 15).into_font())
         .draw()?;
 
-    // And we can draw something in the drawing area
-    chart.draw_series(LineSeries::new(
-        vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
-        &RED,
-    ))?;
-    // Similarly, we can draw point series
-    chart.draw_series(PointSeries::of_element(
-        vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
-        5,
-        &RED,
-        &|c, s, st| {
-            return EmptyElement::at(c)    // We want to construct a composed element on-the-fly
-            + Circle::new((0,0),s,st.filled()) // At this point, the new pixel coordinate is established
-            + Text::new(format!("{:?}", c), (10, 0), ("Arial", 10).into_font());
-        },
-    ))?;
+    let data = points;
+
+    println!("Data: {:?}", data);
+    chart.draw_series(
+        Histogram::vertical(&chart)
+            .style(RED.mix(0.5).filled())
+            .data(data.iter().map(|x: &u32| (*x, 1))),
+    )?;
+
     Ok(())
 }
 
 
-fn map_tasks_to_days_ago(tasks: HashSet<Task>) -> HashMap<i32, i32> {
-    let mut map: HashMap<i32, i32> = HashMap::new();
+fn map_tasks_to_days_ago(tasks: HashSet<Task>) -> HashMap<u32, u32> {
+    let mut map: HashMap<u32, u32> = HashMap::new();
     let utc_now: DateTime<Utc> = Utc::now();
     println!("tasks size in map fn is {}", tasks.len());
     for t in tasks.iter() {
@@ -183,19 +177,19 @@ fn map_tasks_to_days_ago(tasks: HashSet<Task>) -> HashMap<i32, i32> {
 
                 // 86400 seconds in a day
 
-                let mut days_ago_counter: i32 = 0;
+                let mut days_ago_counter: u32 = 0;
 
                 while time_diff > std::time::Duration::from_secs(86400) {
                     time_diff = time_diff - std::time::Duration::from_secs(86400);
                     days_ago_counter = days_ago_counter + 1;
                 }
 
-                let days_ago: i32 = days_ago_counter;
+                let days_ago: u32 = days_ago_counter;
 
                 if !map.contains_key(&days_ago) {
                     map.insert(days_ago, 0);
                 }
-                let current_count: &mut i32 = map.get_mut(&days_ago).unwrap();
+                let current_count: &mut u32 = map.get_mut(&days_ago).unwrap();
                 *current_count += 1;
             }
         }
