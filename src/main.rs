ce5e9075adfa32706c4940de629e21ca6283d517
= <jake@vossen.dev>
2019-10-11 14:18:00 -0600

    getting points as vec of tuples

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use time::Duration;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
+use plotters::prelude::*;
+
 use csv::{ReaderBuilder, StringRecord};
 use chrono::prelude::*;
 
@@ -93,12 +95,62 @@ fn main() {
     let mut days_ago_to_task_count: HashMap<i32, i32> = HashMap::new();
     days_ago_to_task_count = map_tasks_to_days_ago(tasks);
 
-    println!("len of days ago to task count: {}", days_ago_to_task_count.len());
+    let points: Vec<(i32, i32)> = get_points(days_ago_to_task_count, days_requested);
 
-    // for (days, count) in &days_ago_to_task_count {
-        // println!("{}: {}", days, count);
-    // }
+    // draw_data();
 }
+
+fn get_points(map: HashMap<i32, i32>, days_requested: i32) -> Vec<(i32, i32)> {
+    let mut points: Vec<(i32, i32)> = Vec::new();
+    for x in 0..days_requested {
+        points.push((x, *map.get(&x).unwrap_or(&0)));
+    }
+    return points;
+}
+
+fn draw_data(points: Vec<(i32, i32)>, max_point: i32) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("stats.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE);
+    let root = root.margin(10, 10, 10, 10);
+    // After this point, we should be able to draw construct a chart context
+    let mut chart = ChartBuilder::on(&root)
+        // Set the caption of the chart
+        // .caption("This is our first plot", ("Arial", 40).into_font())
+        // Set the size of the label region
+        .x_label_area_size(20)
+        .y_label_area_size(40)
+        // Finally attach a coordinate on the drawing area and make a chart context
+        .build_ranged(0f32..10f32, 0f32..10f32)?;
+
+    // Then we can draw a mesh
+    chart
+        .configure_mesh()
+        // We can customize the maximum number of labels allowed for each axis
+        .x_labels(5)
+        .y_labels(5)
+        // We can also change the format of the label text
+        .y_label_formatter(&|x| format!("{:.3}", x))
+        .draw()?;
+
+    // And we can draw something in the drawing area
+    chart.draw_series(LineSeries::new(
+        vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
+        &RED,
+    ))?;
+    // Similarly, we can draw point series
+    chart.draw_series(PointSeries::of_element(
+        vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
+        5,
+        &RED,
+        &|c, s, st| {
+            return EmptyElement::at(c)    // We want to construct a composed element on-the-fly
+            + Circle::new((0,0),s,st.filled()) // At this point, the new pixel coordinate is established
+            + Text::new(format!("{:?}", c), (10, 0), ("Arial", 10).into_font());
+        },
+    ))?;
+    Ok(())
+}
+
 
 fn map_tasks_to_days_ago(tasks: HashSet<Task>) -> HashMap<i32, i32> {
     let mut map: HashMap<i32, i32> = HashMap::new();
